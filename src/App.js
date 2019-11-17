7f19493588daa3ba3b38120e4c8ddd9e71149284
FlorianSahbi <florian.sahbi@gmail.com>
2019-11-17 23:16:02 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,111 @@
+import React, { useState, useRef, useEffect } from 'react';
+import './App.css';
+import pro from "./promare.png"
+import pro2 from "./pro2.png"
+import logo from "./Logo_Promare.png"
+import sound from "./nexus.mp3"
+
+
+const demos = [
+  {
+    one: {
+      bg: "http://caraujo_pens.surge.sh/images/ZnkfwAR.jpg",
+      bm: "none"
+    },
+    two: {
+      bg: "http://caraujo_pens.surge.sh/images/ONBXKkS.jpg",
+      bm: "none"
+    },
+    three: {
+      bg: "http://caraujo_pens.surge.sh/images/qKNpn9G.png",
+      bm: "none"
+    }
+  },
+  {
+    one: {
+      bg: pro,
+      bm: "none"
+    },
+    two: {
+      bg: pro,
+      bm: "multiply"
+    },
+    three: {
+      bg: pro,
+      bm: "multiply"
+    }
+  },
+  {
+    one: {
+      bg: "http://caraujo_pens.surge.sh/images/YpTSfYD.jpg",
+      bm: "none"
+    },
+    two: {
+      bg: "http://caraujo_pens.surge.sh/images/ziAdjNM.jpg",
+      bm: "multiply"
+    },
+    three: {
+      bg: "http://caraujo_pens.surge.sh/images/dnfONso.png",
+      bm: "none"
+    }
+  }
+];
+
+function App() {
+
+  let timer = 0;
+  const [demo, setDemo] = useState(demos[1]);
+  const [active, setActive] = useState(0);
+  setInterval(() => {
+    timer++
+    console.log(timer)
+  }, 1000);
+
+  const ref = useRef(null)
+
+  useEffect(() => {
+    ref.current.currentTime = timer;
+    ref.current.play();
+  }, [])
+
+  document.body.setAttribute("data-demo", 2);
+
+
+  const handleClick = (e, value) => {
+    e.preventDefault();
+    setDemo(demos[value])
+    setActive(value);
+    document.body.setAttribute("data-demo", value + 1);
+  }
+
+  const start = (e) => {
+    e.preventDefault();
+    ref.current.currentTime = timer;
+    ref.current.play();
+    ref.current.muted = false;
+    e.target.style.display = "none";
+  }
+
+  return (
+    <>
+      <div className="changer">
+        <img src={logo} id="title" alt="bla" style={{ width: "100%" }} />
+        <div className="demos">
+          {/* <a onClick={(e) => handleClick(e, 0)} data-go="1" href="https://www.google.com">Galo</a>
+          <a onClick={(e) => handleClick(e, 1)} data-go="2" href="https://www.google.com">Lio</a>
+          <a onClick={(e) => handleClick(e, 2)} data-go="3" href="https://www.google.com">knowledge</a> */}
+          <a onClick={(e) => start(e)} data-go="2" href="https://www.google.com">Start</a>
+
+        </div>
+      </div>
+      <div id="container">
+        <div id="f1" style={{ backgroundImage: `url(${demo.one.bg})`, mixBlendMode: `${demo.one.bm}` }}></div>
+        {/* <div id="f2" style={{ backgroundImage: `url(${demo.two.bg})`, mixBlendMode: `${demo.two.bm}` }}></div>
+        <div id="f3" style={{ backgroundImage: `url(${demo.three.bg})`, mixBlendMode: `${demo.three.bm}` }}></div> */}
+      </div>
+      <audio src={sound} ref={ref} autoPlay muted controls />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Promare.js
@@ -0,0 +1,135 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import pro from "./promare.png"
+import pro2 from "./pro2.png"
+import logo from "./Logo_Promare.png"
+
+function App() {
+  let demos = [];
+
+  const cloudsuit = {
+    one: {
+      bg: "http://caraujo_pens.surge.sh/images/ZnkfwAR.jpg",
+      bm: "none"
+    },
+    two: {
+      bg: "http://caraujo_pens.surge.sh/images/ONBXKkS.jpg",
+      bm: "lighten"
+    },
+    three: {
+      bg: "http://caraujo_pens.surge.sh/images/qKNpn9G.png",
+      bm: "multiply"
+    }
+  }
+  demos.push(cloudsuit);
+
+  // const urbansky = {
+  //   one: {
+  //     bg: "http://caraujo_pens.surge.sh/images/l1MS7kk.jpg",
+  //     bm: "none"
+  //   },
+  //   two: {
+  //     bg: "http://caraujo_pens.surge.sh/images/6QBLNy6.jpg",
+  //     bm: "multiply"
+  //   },
+  //   three: {
+  //     bg: "http://caraujo_pens.surge.sh/images/bmnzJRs.png",
+  //     bm: "none"
+  //   }
+  // }
+
+  const urbansky = {
+    one: {
+      bg: pro,
+      bm: "none"
+    },
+    two: {
+      bg: pro,
+      bm: "multiply"
+    },
+    three: {
+      bg: pro,
+      bm: "multiply"
+    }
+  }
+  demos.push(urbansky);
+
+  const knowledge = {
+    one: {
+      bg: "http://caraujo_pens.surge.sh/images/YpTSfYD.jpg",
+      bm: "none"
+    },
+    two: {
+      bg: "http://caraujo_pens.surge.sh/images/ziAdjNM.jpg",
+      bm: "multiply"
+    },
+    three: {
+      bg: "http://caraujo_pens.surge.sh/images/dnfONso.png",
+      bm: "none"
+    }
+  }
+  demos.push(knowledge);
+
+
+  const [demo, setDemo] = useState(2);
+  const [active, setActive] = useState(1);
+
+  useEffect(() => {
+    var container = document.getElementById("container");
+    applyDemo(demo, container);
+  })
+
+
+  const applyDemo = (number, ctn) => {
+    let go = demos[number - 1];
+    console.log(active)
+    ctn.innerHTML = "";
+
+
+    const f1 = document.createElement("div");
+    f1.setAttribute("id", "f1");
+    const style1 = "background-image: url(" + go.one.bg + "); mix-blend-mode: " + go.one.bm + ";";
+    f1.setAttribute("style", style1);
+    ctn.appendChild(f1);
+
+
+    const f2 = document.createElement("div");
+    f2.setAttribute("id", "f2");
+    const style2 = "background-image: url(" + go.two.bg + "); mix-blend-mode: " + go.two.bm + ";";
+    f2.setAttribute("style", style2);
+    ctn.appendChild(f2);
+
+    const f3 = document.createElement("div");
+    f3.setAttribute("id", "f3");
+    const style3 = "background-image: url(" + go.three.bg + "); mix-blend-mode: " + go.three.bm + ";";
+    f3.setAttribute("style", style3);
+    ctn.appendChild(f3);
+    setActive(number);
+
+
+  }
+
+
+
+  const handleClick = (e, value) => {
+    e.preventDefault();
+    document.body.setAttribute("data-demo", value);
+    setDemo(value)
+  }
+
+  return (
+    <>
+      <div className="changer">
+        <img src={logo} alt="bla" style={{ width: "100%" }} />
+        <div className="demos">
+          <a onClick={(e) => handleClick(e, 1)} href="https://www.google.com">Galo</a>
+          <a onClick={(e) => handleClick(e, 2)} href="https://www.google.com">Lio</a>
+          <a onClick={(e) => handleClick(e, 3)} href="https://www.google.com">knowledge</a>
+        </div>
+      </div>
+      <div id="container"></div>
+    </>
+  );
+}
+
+export default App;
